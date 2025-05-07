4e85c76470194bc4482f6c87f56f45da231b87ff
IsaacPerod <isaacrodrigues.aluno@unipampa.edu.br>
2025-05-07 15:31:48 -0300

    zip trabalho

--- src/main/java/com/alegretelocacoes/interfaces/ILocadoraLista.java
+++ src/main/java/com/alegretelocacoes/interfaces/ILocadoraLista.java
@@ -1,9 +1,3 @@
-/**
- * Interface defining operations for a double-linked list data structure used in a rental system.
- * Provides methods for inserting elements at the beginning and end of the list,
- * removing elements by key, searching elements, printing in both directions,
- * checking if the list is empty and getting its size.
- */
 package interfaces;
 
 public interface ILocadoraLista {
